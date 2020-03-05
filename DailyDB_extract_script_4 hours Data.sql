2cef089b78e11b6b87a18db28d3f87fe73877764
pratyada <prateekyadav2010@gmail.com>
2020-03-04 19:18:28 -0500

    workout on the data visualization and data cleaning

--- DailyDB_extract_script_4 hours Data.sql
+++ DailyDB_extract_script_4 hours Data.sql
@@ -12,5 +12,55 @@ select * from s1voltertp_2020022123) a, tas_master.p_netconf@sqmdb b
 where (DL_PKT_COUNT + UL_PKT_COUNT) >0 and a.CI = b.CELL_ID
 
 
+--final query to select a part of data--
 
-test change
+select to_char(ts_start,'yyyymmdd hh') as sdatehour, b.SITE_NAME, b.NETWORK_TYPE, b.AREA , b.AREA_TYPE, b.AREA2, b.AREA3, b.LONGITUDE, b.LATITUDE , ipadd_dotnumformat(SGW_IP), TS_START, TS_END, 
+TRAFFIC_UL,TRAFFIC_DL,SESSION_END,XDRID,SAMPLE_FREQ,UL_PAYLOAD_DATA_TOTAL_SIZE,UL_PAYLOAD_DATA_SIZE,
+DL_PAYLOAD_DATA_TOTAL_SIZE,DL_PAYLOAD_DATA_SIZE,DIR,CODEC_RATE
+from  (select * from s1voltertp_2020022618 union all
+select * from s1voltertp_2020022619 union all
+select * from s1voltertp_2020022620 union all
+select * from s1voltertp_2020022621 union all
+select * from s1voltertp_2020022622 union all
+select * from s1voltertp_2020022623) a, tas_master.p_netconf@sqmdb b
+where (DL_PKT_COUNT + UL_PKT_COUNT) >0 and a.CI = b.CELL_ID
+
+
+select to_char(ts_start,'yyyymmdd hh') as sdatehour, b.SITE_NAME, b.NETWORK_TYPE, b.AREA , b.AREA_TYPE, b.AREA2, b.AREA3, b.LONGITUDE, b.LATITUDE , ipadd_dotnumformat(SGW_IP), TS_START, TS_END, 
+TRAFFIC_UL,TRAFFIC_DL,SESSION_END,XDRID,SAMPLE_FREQ,UL_PAYLOAD_DATA_TOTAL_SIZE,UL_PAYLOAD_DATA_SIZE,
+DL_PAYLOAD_DATA_TOTAL_SIZE,DL_PAYLOAD_DATA_SIZE,DIR,CODEC_RATE
+from  (select * from s1voltertp_2020022700 union all
+select * from s1voltertp_2020022701 union all
+select * from s1voltertp_2020022702 union all
+select * from s1voltertp_2020022703 union all
+select * from s1voltertp_2020022704 union all
+select * from s1voltertp_2020022705 union all
+select * from s1voltertp_2020022706 union all
+select * from s1voltertp_2020022707 union all
+select * from s1voltertp_2020022708) a, tas_master.p_netconf@sqmdb b
+where (DL_PKT_COUNT + UL_PKT_COUNT) >0 and a.CI = b.CELL_ID
+
+select to_char(ts_start,'yyyymmdd hh') as sdatehour, b.SITE_NAME, b.NETWORK_TYPE, b.AREA , b.AREA_TYPE, b.AREA2, b.AREA3, b.LONGITUDE, b.LATITUDE , ipadd_dotnumformat(SGW_IP), TS_START, TS_END, 
+TRAFFIC_UL,TRAFFIC_DL,SESSION_END,XDRID,SAMPLE_FREQ,UL_PAYLOAD_DATA_TOTAL_SIZE,UL_PAYLOAD_DATA_SIZE,
+DL_PAYLOAD_DATA_TOTAL_SIZE,DL_PAYLOAD_DATA_SIZE,DIR,CODEC_RATE
+from  (select * from s1voltertp_2020022709 union all
+select * from s1voltertp_2020022710 union all
+select * from s1voltertp_2020022711 union all
+select * from s1voltertp_2020022712 union all
+select * from s1voltertp_2020022713 union all
+select * from s1voltertp_2020022714 union all
+select * from s1voltertp_2020022715 union all
+select * from s1voltertp_2020022716 union all
+select * from s1voltertp_2020022717 union all
+select * from s1voltertp_2020022718) a, tas_master.p_netconf@sqmdb b
+where (DL_PKT_COUNT + UL_PKT_COUNT) >0 and a.CI = b.CELL_ID
+
+select to_char(ts_start,'yyyymmdd hh') as sdatehour, b.SITE_NAME, b.NETWORK_TYPE, b.AREA , b.AREA_TYPE, b.AREA2, b.AREA3, b.LONGITUDE, b.LATITUDE , ipadd_dotnumformat(SGW_IP), TS_START, TS_END, 
+TRAFFIC_UL,TRAFFIC_DL,SESSION_END,XDRID,SAMPLE_FREQ,UL_PAYLOAD_DATA_TOTAL_SIZE,UL_PAYLOAD_DATA_SIZE,
+DL_PAYLOAD_DATA_TOTAL_SIZE,DL_PAYLOAD_DATA_SIZE,DIR,CODEC_RATE
+from  (select * from s1voltertp_2020022719 union all
+select * from s1voltertp_2020022720 union all
+select * from s1voltertp_2020022721 union all
+select * from s1voltertp_2020022722 union all
+select * from s1voltertp_2020022723) a, tas_master.p_netconf@sqmdb b
+where (DL_PKT_COUNT + UL_PKT_COUNT) >0 and a.CI = b.CELL_ID
